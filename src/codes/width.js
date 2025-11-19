a9d5155a21929264fc0377335d54d359848eaf70
CiroLee <ciromail@126.com>
2025-11-19 22:06:47 +0800

    feat: add min-width demo

--- src/codes/width.js
+++ src/codes/width.js
@@ -5,3 +5,18 @@ export const widthBasicCode = `<div w="96">w-96</div>
 <div w="40">w-40</div>
 <div w="32">w-40</div>
 <div w="24">w-24</div>`;
+
+export const sizeCode = `<div size="16">size="16"</div>
+<div size="20">size="20"</div>
+<div size="24">size="24"</div>
+<div size="32">size="32"</div>
+<div size="40">size="40"</div>`;
+
+export const minWidthCode = `<div w="20">
+  <div min-w="80">min-w="80"</div>
+  <div min-w="64">min-w="64"</div>
+  <div min-w="48">min-w="48"</div>
+  <div min-w="40">min-w="40"</div>
+  <div min-w="32">min-w="32"</div>
+  <div min-w="24">min-w="24"</div>
+</div>`;
